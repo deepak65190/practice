f0ca41610534f4cd49957f46a88e52ae80a992d3
deepak65190 <deepakbisht9773724383@gmail.com>
2023-07-10 16:47:56 +0530

    mvc set up

--- blogLive/backend/index.js
+++ blogLive/backend/index.js
@@ -2,8 +2,17 @@ require('dotenv').config() ;
 const express = require('express')
 const app = express()
 
-
+const connection=require("./config/db")
+const userRouter=require("./routes/user.route") ;
+app.use("/user",userRouter)
 
 app.listen(process.env.PORT ,async()=>{
-    console.log(`listioning on port num ${process.env.PORT}`)
+    await connection
+    try{
+        console.log('connected to db')
+        console.log(`listioning on port num ${process.env.PORT}`)
+    }catch(err){
+        console.log(err.message)
+    }
+    
 })
