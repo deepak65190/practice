dec62d4b342a7de83a5fce104bb03c11f9e1b65d
deepak65190 <deepakbisht9773724383@gmail.com>
2024-01-24 14:24:21 +0530

    utils

--- backend2.2/event/src/index.js
+++ backend2.2/event/src/index.js
@@ -1,6 +1,18 @@
-import dotenv from "dotenv"
-import connectDB from './db/index.js';
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./app.js";
 dotenv.config({
-    path:"./env"
-})
-connectDB() ;
+  path: "./env",
+});
+
+
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`app is listening on port no ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("connection fail !1 ", err);
+  });
