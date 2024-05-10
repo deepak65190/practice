fa5a632131a5add4779ca8348f49f9a39a53b3df
deepak65190 <deepakbisht9773724383@gmail.com>
2024-05-10 22:16:20 +0530

    card

---
+++ card/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Card } from './Card/Card'
+function App() {
+  
+
+  return (
+    <>
+      <Card/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ card/vite-project/src/Card/Card.jsx
@@ -0,0 +1,6 @@
+import React from "react" ;
+export  const Card=()=>{
+    return(
+        <h1>Hellow world</h1>
+    )
+}
