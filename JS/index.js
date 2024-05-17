3a4d3ad177f29309e89d93bfce37ffa28b4375d4
deepak65190 <deepakbisht9773724383@gmail.com>
2024-05-17 23:42:04 +0530

    js output based q & a

---
+++ JS/index.js
@@ -0,0 +1,64 @@
+// let data = "size";
+// const bird = {
+//     size:"small",
+// };
+// //console.warn(bird[data])
+// console.warn(bird['size'])
+// console.warn(bird.size)
+// console.warn(bird.data)
+// var x;
+// let x = 10;
+// console.log(x)
+
+//  var x;
+// var x = 10;
+// console.log(x)
+// let number = 0;
+// console.log(number++);
+// console.log(++number);
+// console.log(number);
+
+//     (()=>{
+//         let x =y= 20;
+//     })(); 
+//     console.log(y)
+// // let data = 10 - -10;
+// console.log(data)
+// let z = [1,2,3,4]
+// let a = {name:"anil"}
+// console.log(typeof ...z) 
+// let arr = [1,2,3,4,5,6]
+// let store = [];
+
+// for(i = 0; i < arr.length; i++){
+//   console.log(arr)
+//   store.push(arr)
+//   arr.push(arr.shift())
+// }
+// console.log('store', JSON.stringify(store))
+
+// console.log( NaN)
+
+// let set = new Map([1,1,2,3,4,5])
+// console.log(typeof set)
+
+// What is the freeze and seal what is the use
+// for(var i=0;i<4;i++){
+// setTimeout(()=>{console.log(i)})
+// setImmediate(()=>{console.log("Hello")})
+// }
+// let i;
+// for( i=0;i<4;i++){
+// setTimeout(()=>{console.log(i)})
+// }
+// console.log("hii")
+// setImmediate(()=>{console.log("Hello")})
+// console.log("5")
+// for(var i=0;i<4;i++){
+//     function a(j){
+//         setTimeout(()=>{console.log(i)})
+//     }
+// a(i)
+// }
+a=10
+console.log(a)
