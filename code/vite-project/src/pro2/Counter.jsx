75456819fc42589af42e709c775287e00010f0f7
deepak65190 <deepakbisht9773724383@gmail.com>
2024-05-23 19:20:41 +0530

    tail wind css

--- code/vite-project/src/pro2/Counter.jsx
+++ code/vite-project/src/pro2/Counter.jsx
@@ -13,7 +13,7 @@ const Counter = () => {
     return (
         <>
             <h1>{counter}</h1>
-            <button onClick={handleAdd}>add</button>
+            <button onClick={handleAdd} className="text-white bg-cyan-500 hover:bg-cyan-600 p-2 rounded-md mx-20">add</button>
         </>
     );
 };
