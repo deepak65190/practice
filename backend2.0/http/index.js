e0fd4a789045112ea23eb01284eed2fb54528f9a
deepak65190 <deepakbisht9773724383@gmail.com>
2024-06-09 14:25:54 +0530

    dns

--- backend2.0/http/index.js
+++ backend2.0/http/index.js
@@ -1,5 +1,8 @@
 const http=require("http") ;
-const fs=require("fs")
+const fs=require("fs") ;
+const cowsay=require("cowsay") ;
+const dns=require("node:dns") ;
+const param=process.argv[2] ;
 const server=http.createServer((req,res)=>{
     if(req.url==="/home"){
         res.end("hello home")
@@ -13,10 +16,25 @@ fs.readFile("./data.json" ,{encoding:"utf-8"},(err ,data)=>{
         res.end(data)
     }
 })
+    }else if(req.url=="/text"){
+        // res.setHeader("Content-Type","text/html") ;
+        res.end("<h1>hello<h1/>")
+    }
+    else if(req.url=="/cow"){
+        res.end(cowsay.think({
+            text:"hello cow",
+            e:"ppp"
+        }))
+    } else if(req.url=="/add"){
+        dns.lookup(param ,(err,address ,family)=>{
+            res.write("the ip addres \n") ;
+            let data=`${address} and f ${family}`
+            res.end(data)
+        })
     }
     else{
 
-        res.end("hello")
+        res.end("hello2")
     }
 })
 server.listen(8080 , ()=>{
