f2e867f52247478ed09f1099a434f5e280bd903d
deepak65190 <deepakbisht9773724383@gmail.com>
2024-07-18 22:17:35 +0530

    aja

--- quizAPP/index.js
+++ quizAPP/index.js
@@ -1,6 +1,9 @@
-let a=20 
-let b=20
-let c=1
-if(a==b==c){
-    console.log(true)
-}else{console.log(false)}
+let a = 20;
+let b = 20;
+let c = 1;
+if ((a == b) == c) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+c=0 ;
