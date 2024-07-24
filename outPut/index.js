634387df0e5962aa2d172ee89748b1f188e7c53d
deepak65190 <deepakbisht9773724383@gmail.com>
2024-07-24 21:45:03 +0530

    ad

--- outPut/index.js
+++ outPut/index.js
@@ -1,6 +1,4 @@
-function bark() {
-  console.log('Woof!');
-  
-}
-
-bark.animal = 'dog';
+let ojb1 = {name: "a", age: 20};
+let ojb2 = {name: "ab", age: 2};
+let obj = Object.assign( {ojb1, ojb2});
+console.log(obj);
