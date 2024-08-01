60c20e8f544c29b47be8d84a5b90b9b38b2a2606
deepak65190 <deepakbisht9773724383@gmail.com>
2024-08-01 16:48:54 +0530

    out put based qa

---
+++ outputBased/index.js
@@ -0,0 +1,42 @@
+/*const bird = {
+    size: 'small',
+  };
+  
+  const mouse = {
+    name: 'Mickey',
+    small: true,
+  };
+  console.log(mouse[bird["size"]])*/
+
+
+ /* function bark() {
+    console.log('Woof!');
+  }
+  
+  bark.animal = 'dog';*/
+
+  /*function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  
+  const lydia = new Person('Lydia', 'Hallie');
+  const sarah = Person('Sarah', 'Smith');
+  
+  //console.log(lydia);
+  console.log(sarah);*/
+
+  /*function getPersonInfo(one, two, three) {
+   // console.log(one);
+     console.log(two);
+    // console.log(three);
+  }
+  
+  const person = 'Lydia';
+  const age = 21;
+  
+  getPersonInfo`${person} is ${age} years old`;*/
+
+
+ let a= [...'Lydia'];
+ console.log(a)
