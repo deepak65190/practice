7dbea9109bcd8530def5c982bcab2037f62b89e0
deepak65190 <deepakbisht9773724383@gmail.com>
2024-08-02 23:58:43 +0530

    output

--- outputBased/index.js
+++ outputBased/index.js
@@ -38,5 +38,65 @@
   getPersonInfo`${person} is ${age} years old`;*/
 
 
- let a= [...'Lydia'];
- console.log(a)
+//  let a= [...'Lydia'];
+//  console.log(a)
+/*function greeting() {
+  throw 'Hello world!';
+}
+
+function sayHi() {
+  try {
+    const data = greeting();
+    console.log('It worked!', data);
+  } catch (e) {
+    console.log('Oh no an error:', e);
+  }
+}
+
+sayHi();*/
+/*function Car() {
+  this.make = 'Lamborghini';
+this.ram="ram"
+}
+
+const myCar = new Car();
+console.log(myCar);*/
+/*(()=>{
+let x=(y=10) 
+console.log(typeof x)
+})()*/
+
+/*class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+Dog.prototype.bark = function() {
+  console.log(`Woof I am ${this.name}`);
+};
+
+const pet = new Dog('Mara');
+
+pet.bark();
+
+delete Dog.bark;
+
+pet.bark();*/
+
+// const set = new Set([1, 1, 2, 3, 4]);
+
+// console.log(set);
+// let a=10
+// function ab(x){
+// console.log(x*2)
+// }
+
+// ab(a)
+// ab(a)
+// index.js
+console.log('running index.js');
+import { sum } from './sum.js';
+console.log(sum(1, 2));
+
+// sum.js
